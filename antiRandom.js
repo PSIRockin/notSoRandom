ce4111ad30c24a6b5e894b83ed10883f688e820b
PSIRockin <jonjrn@gmail.com>
2018-07-13 13:16:55 -0400

    idk

--- antiRandom.js
+++ antiRandom.js
@@ -1,34 +1,33 @@
-const input = process.argv [2]
+const input = process.argv [2];
 
 function random () {
-    const number = Math.floor(Math.random()*input)
-    console.log (number)
+    const number = Math.floor(Math.random()*input);
+    console.log (number);
 }
 
 random ();
 
-const stringinput = process.argv [3]
+const stringinput = process.argv [3];
 
 function randomLetter () {
-    const letter = Math.floor(Math.random()*stringinput.length)
-    console.log (stringinput[letter])
+    const letter = Math.floor(Math.random()*stringinput.length);
+    console.log (stringinput[letter]);
 }
 
 randomLetter ();
 
 function myGuy (chill) {
-    console.log (chill + ", my guy.")
+    console.log (chill + ", my guy.");
 }
 
 myGuy(process.argv[4]);
 
+const string = process.argv[5];
+const n = process.argv[6]
 
-function string_N_times(string, n) {
-    if (n > 0)
-    return string.repeat(n);
-  else
-    return "";
+function string_N_times() {
+    if (n > 0){
+    console.log(string.repeat(n));
+    }
 }
-
-string_N_times(process.argv[5], process.argv[6]);
-
+string_N_times();
